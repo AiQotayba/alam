8abdc36027f5fb656bc4a98a34766463dac97f83
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2023-12-21 19:29:49 +0200

    UpdateCommitAuto

--- src/pages/api/client/course-ads/index.js
+++ src/pages/api/client/course-ads/index.js
@@ -6,7 +6,7 @@ export default async function api_course_ads(req, res, next) {
     app.get(async () => {
         let courses = await CourseAds.find({ display: true })
             .select("-part -register -bio ")
-            .sort({ sort: -1 });
+            .sort({ sort: 1 });
         app.Send(courses);
     });
 }
